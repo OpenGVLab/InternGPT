e978841046d9982b0d868f010b3f2cc8c502f650
laizeqiang <laizeqiang@outlook.com>
2023-05-16 18:22:18 +0800

    add imagebind

---
+++ iGPT/models/imagebind/__init__.py
@@ -0,0 +1,2 @@
+from .data import load_and_transform_text, load_and_transform_audio_data, load_and_transform_video_data, load_and_transform_vision_data
+from .models.imagebind_model import imagebind_huge, ModalityType
