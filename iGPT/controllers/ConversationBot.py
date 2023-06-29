b81916b8d05dac479337108eddfb30e796e42b4d
ZwGao <1557033708qq.com>
2023-06-29 23:33:24 +0800

    add husky e_mode

--- iGPT/controllers/ConversationBot.py
+++ iGPT/controllers/ConversationBot.py
@@ -157,7 +157,7 @@ class ConversationBot:
                 if e.startswith('inference'):
                     func = getattr(instance, e)
                     self.tools.append(Tool(name=func.name, description=func.description, func=func))
-
+        print("Max allocated memory:", torch.cuda.max_memory_allocated())
         print("Current allocated memory:", torch.cuda.memory_allocated())
         print("models",set([type(e).__name__ for e in self.models.values()]))
 
@@ -544,6 +544,8 @@ class ConversationBot:
             
         print(f"\nProcessed run_text, Input text: {text}\nCurrent state: {state}\n"
               f"Current Memory: {agent.memory.buffer}")
+        print("Max allocated memory:", torch.cuda.max_memory_allocated())
+        print("Current allocated memory:", torch.cuda.memory_allocated())
         return state, state, user_state
     
     def run_audio(self, audio_path, state, user_state):
